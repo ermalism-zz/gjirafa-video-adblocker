4946cb05ccb1e0f1b141d7afc435654b797e723c
ermalism <e.ismajli@celonis.de>
2020-08-22 00:55:19 +0200

    0.0.2 version

--- contentscript.js
+++ contentscript.js
@@ -1,26 +1,18 @@
 var div = document.createElement('script');
 div.innerHTML=`
 function removeAd() {
-	videoEvents.ad.firstPlay = !1,
-		document.querySelector(".rkl-button").classList.remove("skip"),
-		videoEvents.ad.active = !1,
-		videoEvents.ad.actionSaved = !1,
-		videoEvents.ad.skipCheck = !1,
-		videoPlayer = document.querySelector("#video"),
-		player.classList.remove("cuepoint"),
-		videoEvents.ad.hls ? videoEvents.ad.hls.destroy() : videoEvents.ad.player.pause(),
-		totalTime && (totalTime.innerText = videoEvents.formatDuration(originalPlayer.dataset.seconds)),
-		console.log("video duration:", videoPlayer.duration),
-		isLive === "True" ? videoPlayer.currentTime = videoPlayer.duration === Infinity || isNaN(videoPlayer.duration) ? hlsPlayer.liveSyncPosition : videoPlayer.duration - 6 : lastUpdateSeconds == 0 ? (videoEvents.handleTimestamp.hasTimestampCheck = !1,
-			videoEvents.handleTimestamp.check(),
-			lastUpdateSeconds = videoPlayer.currentTime) : videoPlayer.currentTime = lastUpdateSeconds - 2 >= 0 ? lastUpdateSeconds - 2 : lastUpdateSeconds,
-		document.querySelector("#video").volume = document.querySelector("#vad").volume,
-		document.querySelector("#video").muted = document.querySelector("#vad").muted,
-		videoEvents.handleTimestamp.check(),
-		videoPlayer.play()
+    typeof gjanout != "undefined" && gjanout.stopVideoAd && gjanout.stopVideoAd("vad");
+    adPlatformEvents.holder.style.display = "none";
+    adPlatformEvents.requesting = !1;
+    adPlatformEvents.active = !1;
+    document.querySelector("#play-embed") && (document.querySelector("#play-embed").style.display = "block");
+    unmuteOverlay.style.visibility = "visible";
+    videoEvents.loadAjaxComponents()
+    videoEvents.play()
 }
+
 function checkForAdRemoval() {
-	if(videoEvents && videoEvents.ad.active == 1) {
+	if(adPlatformEvents.active || adPlatformEvents.requesting) {
         console.log("remove ad");
 		removeAd();
 	}
@@ -30,6 +22,7 @@ var mutationObserver = new MutationObserver(function(mutations) {
 		checkForAdRemoval();
     });
   });
+  
 mutationObserver.observe(document.documentElement, {
   attributes: true,
   characterData: true,
